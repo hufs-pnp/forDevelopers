6cddc577bdc2b6e15e275559b6b74f8c667fa4c8
Kim Seok Yoon <kjkksu2@naver.com>
2022-01-10 20:28:58 +0900

    Update passport.js

--- src/backend/passport.js
+++ src/backend/passport.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.PASSPORT_CALLBACK_URL,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     userDataInGoogle
   )
